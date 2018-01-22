8d7c79f010c694405e49b98bb11f2808273c17b4
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-01-23 01:00:51 +0530

    prog1.c: corrected delete
    delete function can change the list.
    therefore we need to return list for the delete function

--- lab1/gdb_programs/prog1.c
+++ lab1/gdb_programs/prog1.c
@@ -10,17 +10,18 @@ node* insert(node *pointer, int data)
         /* Insert at the beginning.*/        
         /* Allocate memory for the new node and put data in it.*/
         node* tmp = (node *)malloc(sizeof(node));
-        
+
         tmp->data = data;
         tmp->next = pointer;
-        
+
         /* return new head*/
         return tmp;
 }
 int find(node *pointer, int key)
 {
-        
-        /* Iterate through the entire linked list and search for the key. */
+
+        /* Iterate through the entire linked list and search for the 
+         * key. */
         while(pointer!=NULL)
         {
                 if(pointer->data == key) //key is found.
@@ -32,27 +33,34 @@ int find(node *pointer, int key)
         /*Key is not found */
         return 0;
 }
-void delete(node *pointer, int data)
+
+node* delete(node *pointer, int data)
 {
         node *prev=NULL;
-        
+        node *list=pointer;
+
         /* Iterate to find pointer */
         while( (pointer != NULL) && (pointer->data != data ))
         {
                 prev = pointer;
                 pointer = pointer -> next;
         }
+
         if(pointer==NULL)
         {
                 printf("Element %d is not present in the list\n",data);
-                return;
+                return list;
         }
-        
+        else if(prev==NULL) //implies first element
+        {
+          list=pointer->next;
+          free(pointer);
+          return list;
+        }
         /* Set previous pointer to next pointer */
-        prev->next = pointer->next;            
+        prev->next = pointer->next;
         free(pointer);
-       
-        return;
+        return list;
 }
 void print(node *pointer)
 {
@@ -65,7 +73,7 @@ void print(node *pointer)
 }
 int main()
 {
-        
+
         node *list = NULL;
         delete(list,5);
         //inserting elements
@@ -73,24 +81,22 @@ int main()
         list = insert(list, 3);
         list = insert(list, 2);
         list = insert(list, 9);
-        
+
         //printing list
         print(list);
         printf("\n");
-        
+
         //deleting nodes
-        delete(list,3);
-        delete(list,6);
-        delete(list,5);
-        delete(list,9);
-        delete(list,2);
-        delete(list,6);
-        
+        list = delete(list,3);
+        list = delete(list,6);
+        list = delete(list,5);
+        list = delete(list,9);
+        list = delete(list,2);
+        list = delete(list,6);
+
         //print list
         print(list);
         printf("\n");
-        
-        return 0;    
 
-
+        return 0;
 }
